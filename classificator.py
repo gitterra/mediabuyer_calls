38d94c5b623657c3005060f0ba644f7bf85f83a3
gitterra <80519167+gitterra@users.noreply.github.com>
2023-02-15 15:38:02 +0300

    Update classificator.py

--- classificator.py
+++ classificator.py
@@ -22,7 +22,7 @@ CLASS_LIST = ['1_Order','2_Recall','3_Phone','4_Break','5_Prank','6_Auto']
 # Подготовка и запуск предсказания объединенные в функцию
 def predict(text):
   # Преобразую строку в последовательность индексов согласно частотному словарю с помощью токенайзера, формирую в виде разреженной матрицы (bag of words) и отправляю в НС
-  y_pred = model.predict(np.array(tokenizer.sequences_to_matrix(tokenizer.texts_to_sequences([text]))), verbose='0')  
+  y_pred = model.predict(np.array(tokenizer.sequences_to_matrix(tokenizer.texts_to_sequences([text]))), verbose='0')[0]
   res = {CLASS_LIST[i]: round(y_pred[i],4) for i in range(len(CLASS_LIST))}
 
   return res
